577f65a89687f745889f729e4a1d045aecff975f
raquel <obovok@gmail.com>
2014-05-18 21:34:07 -0500

    new calculator requests user inputs

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,19 +1,26 @@
 # tip_calculator revisited exercise
 
-meal = 17.0
-tax = .0825
-tip = .2
+#collecting user input
+meal = raw_input("What is the cost of your meal before tax?")
+tax = raw_input("What is the tax rate on prepared food?")
+tip = raw_input("What percent of your meal cost would you like to leave as tip?")
 
-tax_value = (meal * tax)
-meal_with_tax = (meal + tax_value)
-tip_value = (meal_with_tax * tip)
 
-total = (meal_with_tax + tip)
+#converting user input to floats and percents to decimal
+meal_float = float(meal)
+tax_float = float(tax) / 100
+tip_float = float(tip) / 100
 
-print "Your meal costs $", ("{:.2f}".format(meal_with_tax))
+tax_value = (meal_float * tax_float)
+meal_with_tax = (meal_float + tax_value)
+tip_value = (meal_with_tax * tip_float)
+
+total = (meal_with_tax + tip_value)
+
+print "The cost of your meal before tip is $", ("{:.2f}".format(meal_with_tax))
 print "$", ("{:.2f}".format(tax_value)), " of that amount is tax"
-print "In order to tip 20%, your tip amount should be: $", ("{:.2f}".format(tip_value))
-print "The grand total of your meal (including tax and tip) is: $", ("{:.2f}".format(total))
+print "In order to tip", tip, "%, you will need to add an additional$", ("{:.2f}".format(tip_value))
+print "This will bring the grand total of your meal to $", ("{:.2f}".format(total))
 
 
 
